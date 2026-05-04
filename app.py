a80e204fcde3f57a15bd9ecff4ead7ac0dc97bce
Aaham <aahamkumararya1@gmail.com>
2026-05-04 13:04:19 +0530

    resolving

--- app.py
+++ app.py
@@ -886,7 +886,7 @@ if __name__ == "__main__":
     print("="*60)
     print("🐘 Using PostgreSQL database")
     print("👤 Default Admin: aaham_18 / Aaham@8990")
-    print("🌐 URL: http://127.0.0.1:5000")
     print("="*60 + "\n")
     
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
